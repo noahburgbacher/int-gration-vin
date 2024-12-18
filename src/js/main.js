b4ade367041c26abb4ea14c5e11ca864f6ebabc5
noahburgbacher <noah.burgbacher@studentfr.ch>
2024-12-18 09:37:29 +0100

    ajouter la fonctionalité de quantité au bouton ajouter au panier

--- src/js/main.js
+++ src/js/main.js
@@ -15,3 +15,35 @@ document.addEventListener("DOMContentLoaded", () => {
     loader.classList.add("hidden");
   }, 50);
 });
+
+const boutonAjouterAuPanier = document.querySelector(".ajouter_au_panier");
+let quantite = 0;
+
+boutonAjouterAuPanier.addEventListener("click", () => {
+  if (quantite === 0) {
+    boutonAjouterAuPanier.innerHTML = `
+      <button class="moins bouton quantity-controls">-</button>
+      <span class="quantite bouton quantity-controls">${quantite}</span>
+      <button class="plus bouton quantity-controls">+</button>
+    `;
+    const boutonMoins = boutonAjouterAuPanier.querySelector(".moins");
+    const boutonPlus = boutonAjouterAuPanier.querySelector(".plus");
+    const affichageQuantite = boutonAjouterAuPanier.querySelector(".quantite");
+
+    boutonMoins.addEventListener("click", () => {
+      if (quantite > 0) {
+        quantite--;
+        affichageQuantite.textContent = quantite;
+        if (quantite === 0) {
+          boutonAjouterAuPanier.innerHTML = "Ajouter au panier";
+          boutonAjouterAuPanier.classList.remove("quantity-controls");
+        }
+      }
+    });
+
+    boutonPlus.addEventListener("click", () => {
+      quantite++;
+      affichageQuantite.textContent = quantite;
+    });
+  }
+});
